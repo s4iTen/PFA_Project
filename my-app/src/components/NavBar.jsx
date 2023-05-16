ca236d2943a81d0fa6b3aab61524c4593fe94d61
s4iTen <ychouk102@gmail.com>
2023-05-16 14:00:45 +0200

    this is the last updates

--- my-app/src/components/NavBar.jsx
+++ my-app/src/components/NavBar.jsx
@@ -1,28 +1,27 @@
 import React from 'react';
 import { signOut } from 'firebase/auth';
 import auth from '../firebase';
+import { useState } from 'react';
+import { Helmet } from 'react-helmet';
 
 
-const LoggedInComponent = () => {
-  return (
-    <div>
-      {/* Content of the component */}
-    </div>
-  );
-};
+
+
+
 
 const NavBar = () => {
+  
   const isLoggedIn = !!localStorage.getItem('current user');
-
+  
   const handleSignOut = () => {
     signOut(auth)
-      .then(() => {
-        localStorage.removeItem('current user');
-        window.location.href = '/Login';
-      })
-      .catch((error) => {
-        console.log(error);
-      });
+    .then(() => {
+      localStorage.removeItem('current user');
+      window.location.href = '/';
+    })
+    .catch((error) => {
+      console.log(error);
+    });
   };
 
   const navigateToLogin = () => {
@@ -44,19 +43,43 @@ const NavBar = () => {
           <li>About Us</li>
         </ul>
       </div>
-      {isLoggedIn && <LoggedInComponent />}
       <div className='LoginSignup'>
-        {isLoggedIn ? (
-          <button onClick={handleSignOut}>Sign Out</button>
-        ) : (
+        {!isLoggedIn ? (
           <>
             <button onClick={navigateToLogin}>Log In</button>
             <button onClick={navigateToSignUp}>Sign Up</button>
+            
           </>
+        ) : (
+          <div className='userContainer'>
+            <div className='userTools'>
+              <ul>
+                <li>Profile</li>
+                <li>Likes</li>
+                <button onClick={handleSignOut}>Sign Out</button>
+              </ul>
+            </div>
+          <select name="" id="">
+            <option value="" disabled>Settings</option>
+            <option value="">Settings</option>
+            <option value="">Settings</option>
+            <option value="">Settings</option>
+            <div className='dropdown'>
+                  <ul>
+                    <li>Settings</li>
+                    <li>Account</li>
+                    <li>Likes</li>
+                  </ul>
+                  </div>
+          </select>
+        
+        <button onClick={handleSignOut}>Sign Out</button>
+        </div>
+  
         )}
       </div>
-    </div>
-  );
+      </div>
+  )
 };
 
 export default NavBar;
